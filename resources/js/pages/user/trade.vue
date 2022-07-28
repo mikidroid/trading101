23ed27fda28468d4612d3c3a800cbf809b09d87e
michael uche <michaelphardy00@gmail.com>
2022-07-28 11:18:05 +0100

    updated

--- resources/js/pages/user/trade.vue
+++ resources/js/pages/user/trade.vue
@@ -1,8 +1,51 @@
 <template>
   <guest-layout v-bind="{title:'Trade'}">
  
+    <v-tabs
+       v-model="tabs"
+        left
+        dark
+        :color="color.accent"
+        :background-color="color.p_dark"
+        >
+      <v-tab>
+        Chart
+      </v-tab>
+      <v-tab>
+        Tech Analysis
+      </v-tab>
+      <v-tab>
+        Prices
+      </v-tab>
+      <v-tab>
+        Order book
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items 
+       v-model="tabs" 
+       :style="{backgroundColor:color.p_dark}">
+      <v-tab-item>
+        <vuetradingchart class="mt-5"
+         :options="chartOptions"></vuetradingchart>
+      </v-tab-item>
+      <v-tab-item 
+         :style="{backgroundColor:color.p_dark}" >
+        <div class="text-center">
+          <iframe frameBorder="0" src="html-widgets/tech-analysis.html" width="400" :height="400"
+          >
+          </iframe>
+        </div>
+      </v-tab-item>
+      <v-tab-item 
+         :style="{backgroundColor:color.p_dark}" >
+        <div class="text-center">
+         <!-- import asset include -->
+         <assets/>  
+        </div>
+      </v-tab-item>
+    </v-tabs-items>
+    
 
-<vuetradingchart :options="chartOptions"></vuetradingchart>
 
   </guest-layout> 
 </template>
@@ -11,9 +54,10 @@
 import {colors} from '../../components/config/config.js';
 require("https://s3.tradingview.com/tv.js");
 import vuetradingchart from 'vue-tradingchart';
+import assets from '../inc/assets.vue'
 
 export default {
- components: { vuetradingchart },
+ components: { vuetradingchart,assets },
  methods:{
   
  },
@@ -43,6 +87,7 @@ export default {
             hide_top_toolbar: false,
             interval: 1 // that's mean 4h //
         },
+     tabs:null,
      color:colors,
      form: this.$inertia.form({
 
